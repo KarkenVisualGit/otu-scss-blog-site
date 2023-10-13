fdff4f5622a850f9e21b62e89610ad7477964b89
Karken Turarov <karken_turarov@mail.ru>
2023-10-13 22:18:29 +0600

    add init method

--- src/carousel.js
+++ src/carousel.js
@@ -4,6 +4,19 @@ class Carousel {
     this.autoPlay();
   }
 
+  init() {
+    this.container = document.querySelector(".carousel");
+
+    this.carouselItems = Array.from(
+      document.querySelectorAll(".carousel-item")
+    );
+    this.selectedItem = document.querySelector(".carousel-item_isSelected");
+    this.marker = this.carouselItems.indexOf(this.selectedItem);
+
+    this.bulletNav();
+    this.bindEvents();
+  }
+
   static initCarousel() {
     return new Carousel();
   }
